7d2657d9097c4f9a85a186a3fff5a0f281e4bbb5
dcostat4@gmail.com <dcostat4@gmail.com>
2023-10-12 01:05:37 +0530

    Minor Update

--- codefest/codefest/settings.py
+++ codefest/codefest/settings.py
@@ -72,7 +72,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = True
+
 
 CORS_ALLOW_ALL_ORIGINS = True
 
